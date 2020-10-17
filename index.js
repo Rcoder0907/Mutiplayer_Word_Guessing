0911f8e8070ab62990cb38bacb3fc2d52cef95ee
Rehan Raza <rehan.raza87@gmail.com>
2020-10-17 19:38:10 +0530

    Auth bug fix

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 require('./utils/db')
 const express = require('express')
 const { port } = require('./utils/constants')
+const cors = require('cors')
 const app = express()
 const authController = require('./controllers/auth.controller')
 const { errorHandler } = require('./middlewares/errorHandler.middleware')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 app.use(express.json())
 
 app.use('/', authController)
+app.use(cors())
 
 app.use(errorHandler)
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+http.listen(port, () => {
   console.log(`Server is running at ${port}`)
 })
